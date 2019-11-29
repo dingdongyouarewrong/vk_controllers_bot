2ca07a196c24d953219922f44e1f6d44d7b6df26
Dmitry Nazarov <38593439+dingdongyouarewrong@users.noreply.github.com>
2019-11-29 10:51:33 +0300

    comments update

--- get_comments_from_vk.py
+++ get_comments_from_vk.py
@@ -15,7 +15,7 @@ def auth():
 
 def getDataFromComments(vk, groupID):
 
-    #получаем последний пост в группе
+    #получаем массив постов в группе
     posts = vk.wall.get(owner_id=groupID, offset=1, count=90)
     print("\n")
     data = pd.DataFrame(columns=['text', 'post_id', "date", "day_in_week", "hour","minute", "day_in_month"])
